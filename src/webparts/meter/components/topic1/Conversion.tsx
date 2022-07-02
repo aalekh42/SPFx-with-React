2d1e277da82801263a52d539cd4b28ab76982701
aalekh42 <aalekhpradeep@gmail.com>
2022-07-02 16:04:00 +0530

    Converted excel into json

--- src/webparts/meter/components/topic1/Conversion.tsx
+++ src/webparts/meter/components/topic1/Conversion.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
+import * as XLSX from 'xlsx';
 
 const Conversion: React.FC = () => {
-  const [excelData,setExcelData]= React.useState('');
+  const [excelData,setExcelData]= React.useState(null);
   const [excelFile,setExcelFile]= React.useState(null);
   const [excelFileError,setExcelFileError]= React.useState('');
 
+  console.log("Excel data",excelData);
   //handleFile
   const fileType=['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
   const handleFile=(e)=>{
@@ -27,6 +29,19 @@ const Conversion: React.FC = () => {
     }
   }
 
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(excelFile!==null){
+        //converting excel files to json
+        const workbook=XLSX.read(excelFile,{type:'buffer'});
+        const worksheetName=workbook.SheetNames[0];//saving excel fileName
+        const worksheet=workbook.Sheets[worksheetName];
+        const data= XLSX.utils.sheet_to_json(worksheet);//converting to json
+        setExcelData(data);
+    }else{
+        setExcelData(null);
+    }
+  }
   return (
     <div className="fluid-container conversion-container">
       <div className="row">
@@ -42,13 +57,14 @@ const Conversion: React.FC = () => {
         >
           <h6>Upload Excel Files</h6>
 
-          <form>
+          <form onSubmit={(e)=>handleSubmit(e)}>
             <input
               type="file"
               required
               style={{ border: "2px solid green" }}
               onChange={(e)=>handleFile(e)}
             ></input>
+            {excelFileError}
             <button type="submit" className="btn btn-primary">
               UPLOAD
             </button>
